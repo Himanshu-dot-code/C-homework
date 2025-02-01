26a1fb7f455993903177c7cbe0dcdb66d605f9d0
Himanshu negi <hnegi32015@gmail.com>
2025-02-01 22:24:05 +0530

    done

--- CHAPTER 5 PRACTICE SET/07_problem.c
+++ CHAPTER 5 PRACTICE SET/07_problem.c
@@ -1,13 +1,30 @@
 /* Write a program using function to print the following pattern (first n lines)
  *
  * * *
- * * * * *  */  
+ * * * * *  */
 
 #include <stdio.h>
 
+int main()
+{
 
-int main(){
+    int n = 3;
+    for (int i = 0; i < n; i++)
+    {
+        // this loop runs from i = 0 to 2
+        // for i = 0 ---> print 1 star
+        // for i = 1 ---> print 3 star
+        // for i = 2 ---> print 5 star
+        // no. of stars = (2*i)+1
 
+        for (int j = 0; j < (2 * i + 1); j++)
+        {
+            // this for loop prints 2*i + 1 stars.
 
+            printf("*");
+        }
+        printf("\n");
+        // this printf prints new line.
+    }
     return 0;
 }
